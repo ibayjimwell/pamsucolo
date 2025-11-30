d8bf8b32f1b2d1e60fbf0d5ac95a8108ce4914f2
ibayjimwell <ibayjimwell@gmail.com>
2025-12-01 06:58:33 +0800

    Update 2014_10_12_100000_create_password_resets_table.php

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('password_resets', function (Blueprint $table) {
             $table->id();
-            $table->string('email')->primary();
+            $table->string('email')->index();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
