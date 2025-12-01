0c6ee2e468efe09ce0cc302bd6a88bfecd39cff3
ibayjimwell <ibayjimwell@gmail.com>
2025-12-01 17:00:58 +0800

    Configuring for production

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ class User extends Authenticatable
 {
     // ... traits
     protected $fillable = [
-        'student_id',
+        'student_number',
         'is_admin', // for admin logic
     ];
     // ... hidden attributes
